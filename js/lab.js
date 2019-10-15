e4a6d5519a016bba96ca69906eeba3b583430556
muathshabatat <shabatatmuath@gmail.com>
2019-10-15 09:52:36 +0300

    edit

--- js/lab.js
+++ js/lab.js
@@ -1,6 +1,6 @@
 'use strict';
 var hours = [' 6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-var store = [];
+var cookiesShop = [];
 var Tables = document.getElementById('Table');
 function Header() {
     var tableRow = document.createElement('tr');
@@ -20,21 +20,21 @@ function Store(name, min, max, avg) {
     this.min = min;
     this.max = max;
     this.avg = avg;
-    this.avgCustArray = [];
+    this.avgArray = [];
     this.cookiesEachHourArray = [];
     this.totalCookies = 0;
-    store.push(this);
+    cookiesShop.push(this);
 }
 Store.prototype.CustHourly = function () {
     for (var v = 0; v <= hours.length; v++) {
         var singleh = Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
-        this.avgCustArray.push(singleh);
+        this.avgArray.push(singleh);
     }
 };
 Store.prototype.cookiesPerCustomer = function () {
     this.CustHourly();
     for (var x = 0; x < hours.length; x++) {
-        var hourCookies = Math.ceil(this.avgCustArray[x] * this.avg);
+        var hourCookies = Math.ceil(this.avgArray[x] * this.avg);
         this.cookiesEachHourArray.push(hourCookies);
         this.totalCookies += hourCookies;
     }
@@ -55,14 +55,14 @@ Store.prototype.render = function () {
     trName.appendChild(tdTotal);
     Tables.appendChild(trName);
 };
-var Seattle = new Store('Seattle', 2, 16, 4.6);
+var Seattle = new Store('Seattle',23, 65, 6.3);
 var Tokyo = new Store('Tokyo', 3, 24, 1.2);
 var Dubai = new Store('Dubai', 11, 38, 3.7);
 var Paris = new Store('Paris', 20, 38, 2.3);
-var Lima = new Store('Lima', 23, 65, 6.3);
+var Lima = new Store('Lima',  2, 16, 4.6);
 function renderAllStores() {
-    for (var i = 0; i < store.length; i++) {
-        store[i].render();
+    for (var i = 0; i < cookiesShop.length; i++) {
+        cookiesShop[i].render();
     }
 };
 function newRow() {
@@ -72,9 +72,9 @@ function newRow() {
     var hTotal = 0;
     for (var i = 0; i < hours.length; i++) {
         var hourlyTotal = 0;
-        for (var j = 0; j < store.length; j++) {
-            hourlyTotal = hourlyTotal + store[j].cookiesEachHourArray[i];
-            hTotal += store[j].cookiesEachHourArray[i];
+        for (var j = 0; j < cookiesShop.length; j++) {
+            hourlyTotal = hourlyTotal + cookiesShop[j].cookiesEachHourArray[i];
+            hTotal += cookiesShop[j].cookiesEachHourArray[i];
             console.log(hTotal);
         }
         var tdElement = document.createElement('td');
